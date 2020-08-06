ee423de372f5ec143ac43575e91756b92982af9e
sayyory <65091436+sayyory@users.noreply.github.com>
2020-08-05 20:33:04 -0500

    Update GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -2,7 +2,7 @@ package utility;
 
 public class GitFun {
     public static void main(String[] args) {
-        System.out.println("Everything is fun");
+        System.out.println("Everything is fun changes changes");
 
     }
 
